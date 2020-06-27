ba74d9748855d617cd9022a9c2d7683125473328
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-06-28 00:04:26 +0100

    Some potential bugfixes

--- lambdas/getRegionsLambda/app.js
+++ lambdas/getRegionsLambda/app.js
@@ -3,45 +3,47 @@ const mysql = require('mysql2');
 // https://github.com/sidorares/node-mysql2/issues/489
 require('iconv-lite').encodingExists('foo');
 
-exports.handler = async function (event) {
-    if (process.env.localTest) {
-        var rds_password = process.env.RDS_PASSWORD
-    }
-    else {
-        var AWS = require('aws-sdk');
-        AWS.config.update({ region: 'eu-west-2' });
-        var ssm = new AWS.SSM();
-        var rds_password = await new Promise(function (success, reject) {
-              ssm.getParameter({
-                Name: process.env.RDS_PASSWORD_PARAM,
-                WithDecryption: true}, function (err, data) {
-                if (err) {
-                  reject(JSON.stringify(err));
-                } else {
-                  success(data["Parameter"]["Value"]);
-                }
-              });
+global.pool = null
+
+exports.handler = async function (event, context, callback) {
+    if (!global.pool) {
+        if (process.env.localTest) {
+            var rds_password = process.env.RDS_PASSWORD
+        }
+        else {
+            var AWS = require('aws-sdk');
+            AWS.config.update({ region: 'eu-west-2' });
+            var ssm = new AWS.SSM();
+            var rds_password = await new Promise(function (success, reject) {
+                ssm.getParameter({
+                    Name: process.env.RDS_PASSWORD_PARAM,
+                    WithDecryption: true
+                }, function (err, data) {
+                    if (err) {
+                        reject(JSON.stringify(err));
+                    } else {
+                        success(data["Parameter"]["Value"]);
+                    }
+                });
             });
-    }
-    var regions = await new Promise((resolve, reject) => {
-        const connection = mysql.createConnection({
+        }
+        global.pool = mysql.createPool({
             host: process.env.RDS_ENDPOINT,
             user: process.env.RDS_USERNAME,
             password: rds_password,
             database: process.env.RDS_DATABASE
         });
-        connection.query(
-            'select DISTINCT RegionName from main',
-            (err, result) => {
-                connection.destroy()
-                return err ? reject(err) : resolve(result);
-            }
-        );
-    });
-
-    var finalRegions = []
-    for (var i = 0; i < regions.length; i++) {
-        finalRegions.push(regions[i].RegionName)
     }
-    return finalRegions
-};
+    if (!process.env.localTest) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    }
+    return await new Promise((resolve, reject) => {
+        global.pool.getConnection(function (err, connection) {
+            return connection.query('select DISTINCT RegionName from main', function (error, results, fields) {
+                // And done with the connection.
+                connection.release();
+
+                return error ? reject(error) : resolve(results);
+            })
+    });
+    })}
--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,10 @@
 const lambda = require('../lambdas/getRegionsLambda/app');
+const log = require('why-is-node-running');
 process.env.localTest = true
 describe('Test getRegionsLambda', function () {
-    it('Verifies successful response', async () => {
+    it('Verifies successful response', async done => {
         const result = await lambda.handler();
-        expect(result[0]).toEqual("Aberdeenshire");
+        expect(result[0]).toEqual({"RegionName": "Aberdeenshire"});
+        done()
     });
 });
