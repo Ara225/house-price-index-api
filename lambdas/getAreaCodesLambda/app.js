7d695369679348468816add397148332ea1c6a9c
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-06-28 19:43:39 +0100

    Add areacode Lambda

---
+++ lambdas/getAreaCodesLambda/app.js
@@ -0,0 +1,57 @@
+const mysql = require('mysql2');
+const AWS = require('aws-sdk');
+
+// Hack to make iconv load the encodings module, otherwise jest crashes. Compare
+// https://github.com/sidorares/node-mysql2/issues/489
+require('iconv-lite').encodingExists('foo');
+
+exports.handler = async (event) => {
+    // Get password from an env var if running locally
+    if (process.env.localTest) {
+        var rds_password = process.env.RDS_PASSWORD
+    }
+    else {
+        // Get password from System Manager Parameter Store if running on AWS
+        AWS.config.update({ region: 'eu-west-2' });
+        var ssm = new AWS.SSM();
+        var rds_password = await new Promise(function (success, reject) {
+            ssm.getParameter({
+                Name: process.env.RDS_PASSWORD_PARAM,
+                WithDecryption: true
+            }, function (err, data) {
+                if (err) {
+                    reject(JSON.stringify(err));
+                } else {
+                    success(data["Parameter"]["Value"]);
+                }
+            });
+        });
+    }
+    var connection = mysql.createConnection({
+        host: process.env.RDS_ENDPOINT,
+        user: process.env.RDS_USERNAME,
+        password: rds_password,
+        database: process.env.RDS_DATABASE
+    })
+    var query = 'select DISTINCT AreaCode from main'
+    connection.connect()
+
+    return new Promise((resolve, reject) => {
+        connection.query(query, function (error, results, fields) {
+            connection.end(err => {
+                if (err) {
+                    return reject(err)
+                }
+                var AreaCodes = []
+                for (var i = 0; i < results.length; i++) {
+                    AreaCodes.push(results[i].AreaCode)
+                }
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(AreaCodes),
+                }
+                resolve(response)
+            })
+        })
+    })
+}
--- test/api.test.js
+++ test/api.test.js
@@ -1,9 +1,18 @@
-const lambda = require('../lambdas/getRegionsLambda/app');
+const getRegionsLambda = require('../lambdas/getRegionsLambda/app');
+const getAreaCodesLambda = require('../lambdas/getAreaCodesLambda/app');
+
 process.env.localTest = true
 describe('Test getRegionsLambda', function () {
     it('Verifies successful response', async done => {
-        const result = await lambda.handler();
+        const result = await getRegionsLambda.handler();
         expect(JSON.parse(result.body)[0]).toEqual("Aberdeenshire");
+        done()
+    });
+});
+describe('Test getAreaCodesLambda', function () {
+    it('Verifies successful response', async done => {
+        const result = await getAreaCodesLambda.handler();
+        expect(JSON.parse(result.body)[0]).toEqual("S12000034");
         done()
     });
 });
